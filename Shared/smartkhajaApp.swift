8c6b18f41c9fe9af0b4adefb1de759c1af1c5145
coder-Aayush <bhattaraiaayush08@gmail.com>
2022-07-05 13:38:54 +0545

    Initial Commit

---
+++ Shared/smartkhajaApp.swift
@@ -0,0 +1,17 @@
+//
+//  smartkhajaApp.swift
+//  Shared
+//
+//  Created by Fatdog on 05/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct smartkhajaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
